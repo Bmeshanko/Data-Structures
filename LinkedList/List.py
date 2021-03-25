8ed67166222a5fb1ce27880b24de487f1d6e8927
Bmeshanko <bmeshank@purdue.edu>
2021-03-25 12:55:21 -0400

    Wrote Constructors in List.py

--- LinkedList/List.py
+++ LinkedList/List.py
@@ -1,1 +1,34 @@
+class Node:
+
+    def __init__(self):
+        self.data = (int) (1 + 10 * random.random());
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self, length):
+        self.head = None
+        self.length = length;
+
+
+    def generateList(num):
+        head = Node();
+        
+        self.head = head;
+        self.length = num;
+
+        for (x in range(num):
+            newNode = Node()
+            traverse = head
+
+            if (head.next == None):
+                head.next = newNode
+
+            while (traverse.next != None):
+                traverse = traverse.next
+
+            traverse.next = newNode;
+            newNode.next = None;
+
+    
 
