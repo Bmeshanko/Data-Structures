ec3120fab7d521f3b9ab27d4394a5fdea2e05ef1
Bmeshanko <bmeshank@purdue.edu>
2021-03-24 23:26:29 -0400

    Used the Node Class instead of just one class.

--- List.java
+++ List.java
@@ -1,20 +1,29 @@
 public class List {
 	
-	private List previous;
-	private List next;
+	private Node head;
 	
-	private int value;
-	
-	public List() {
-		value = (int) (1 + 10 * Math.random());
-	}
-	
-	public void setPrevious(List list) {
-		this.previous = list;
-	}
-	
-	public void setNext(List list) {
-		this.next = list;
+	/* 
+	 * Node Class: Defines a Single Node in the Linked List.
+	 * */
+	public class Node {
+		
+		private Node next;
+		private Node prev;
+		private int value;
+		
+		
+		// Chooses a random value 1 - 10 to be the Node's value.
+		public Node() {	
+			value = (int) (1 + 10 * Math.random());
+		}
+		
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		
+		public void setPrev(Node next) {
+			this.prev = prev;
+		}
 	}
 	
 }
