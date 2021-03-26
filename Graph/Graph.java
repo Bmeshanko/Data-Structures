3e7e5abde7b36879ca0360c9e16cdda71d815ea5
Bmeshanko <bmeshank@purdue.edu>
2021-03-26 16:55:32 -0400

    Added support to make edges

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -2,13 +2,25 @@ import java.util.ArrayList;
 
 public class Graph {
 	
-	private boolean[][] adj;
+	private boolean[][] connections;
 	private int nodes;
 
 	public Graph(int nodes) {
 		this.nodes = nodes;
+		
+		// Initialize the array of connections:
+		this.connections = new boolean[nodes][nodes];
 	}
 	
+	public void oneWayEdge(int from, int to) {
+		this.connections[from][to] = true;
+	}
+
+	public void twoWayEdge(int node1, int node2) {
+		this.connections[node1][node2] = true;
+		this.connections[node2][node1] = true;
+	}
+
 	
 	
 }
