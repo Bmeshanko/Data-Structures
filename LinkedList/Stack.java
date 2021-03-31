0b22f5f0dc45232fd9ae62217c2fcc5b48f5995a
Bmeshanko <bmeshank@purdue.edu>
2021-03-31 10:22:37 -0400

    Wrote add and remove methods for Stack.java

--- LinkedList/Stack.java
+++ LinkedList/Stack.java
@@ -7,4 +7,20 @@ public class Stack extends List {
 	public void generateList(int length) {
 		super.generateList(length);
 	}
+
+	public void add(Node node) {
+		this.length++;
+
+		node.next = this.head;
+		this.head = node;
+	}
+
+	public Node remove() {
+		this.length--;
+
+		Node node = this.head;
+		this.head = this.head.next;
+
+		return node;
+	}
 }
