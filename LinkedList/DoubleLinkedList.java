f1256d40eb1817677d83974027fbf7ea1eb9ff95
Bmeshanko <bmeshank@purdue.edu>
2021-03-31 10:40:26 -0400

    Wrote DoubleLinkedList.java, a doubly Linked List.

---
+++ LinkedList/DoubleLinkedList.java
@@ -0,0 +1,12 @@
+public class DoubleLinkedList {
+	
+	private DoubleNode head;
+	private int length;
+
+	public class DoubleNode() {
+		private int value;
+		private DoubleNode next;
+		private DoubleNode prev;
+	}
+
+}
