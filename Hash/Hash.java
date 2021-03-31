d7231e04bab08cc0c9629912204bcd204e099318
Bmeshanko <bmeshank@purdue.edu>
2021-03-30 22:01:56 -0400

    Realized that I just have a single node.

--- Hash/Hash.java
+++ Hash/Hash.java
@@ -3,8 +3,12 @@ public class Hash {
 	private Object key;
 	private Object value;
 
+	private Hash next;
+
 	public Hash(Object key, Object value) {
 		this.key = key;
 		this.value = value;
 	}
+
+
 }
