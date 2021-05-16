28aaf51f2f4fbdc9ff3bd5808eb9c1535e873da0
Bmeshanko <bmeshank@purdue.edu>
2021-05-15 21:21:21 -0400

    merge_sort.c

---
+++ SortingAlgorithms/merge_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void sort(int *arr, int a, int b) {
+	
+}
+
+void merge(int *arr, int a, int b) {
+	int *doubled_arr = malloc(sizeof(arr) * 4);
+}
+
+int main(int argc, char **argv) {
+		
+	int *arr = malloc(32);
+
+	arr[0] = 2;
+	arr[1] = 4;
+	arr[2] = 8;
+	arr[3] = 6;
+	arr[4] = 3;
+	arr[5] = 1;
+	arr[6] = 5;
+	arr[7] = 7;
+
+}
