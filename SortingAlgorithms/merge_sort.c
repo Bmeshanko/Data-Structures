3e493f428097d4ec026726c0fe0ff1fc1eb8c221
Bmeshanko <bmeshank@purdue.edu>
2021-05-15 22:05:38 -0400

    Polishing merge sort

--- SortingAlgorithms/merge_sort.c
+++ SortingAlgorithms/merge_sort.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void merge(int *arr, int a, int b);
 
 void sort(int *arr, int a, int b) {
 	
 	if (a < b) {
-		int mid = (a + b) / 2;
+		int m = (a + b) / 2;
 
 		sort(arr, a, m);
 		sort(arr, m + 1, b);
@@ -16,17 +19,15 @@ void sort(int *arr, int a, int b) {
 
 void merge(int *arr, int a, int b) {
 	int *new_arr = malloc(sizeof(arr));
-	
-	int half_len = sizeof(arr) / 8;
-	int full_len = half_len * 2;
 
-	int c1 = 0;
-	int c2 = half_len;
+	int c1 = a;
+	int c2 = (a + b) / 2;
 
-	for (int i = 0; i < sizeof(arr); i++) {
-		if (c1 == half_len) {
+	for (int i = a; i < b; i++) {
+        printf("%d ", arr[i]);
+		if (c1 == (a + b) / 2) {
 			new_arr[i] = arr[c2++];
-		} else if (c2 == full_len) {
+		} else if (c2 == a + b) {
 			new_arr[i] = arr[c1++];
 		} else {
 			if (arr[c1] < arr[c2]) {
@@ -41,8 +42,13 @@ void merge(int *arr, int a, int b) {
 }
 
 int main(int argc, char **argv) {
-		
+
 	int arr[8] = {2, 4, 8, 6, 3, 1, 5, 7};
 	
+	printf("%d %d %d %d\n", arr[0], arr[1], arr[2], arr[3]);
+
 	sort(arr, 0, 7);
+
+	printf("%d %d %d %d", arr[0], arr[1], arr[2], arr[3]);
 }
+
