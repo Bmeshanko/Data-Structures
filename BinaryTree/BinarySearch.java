99f968235631b33fd33ae5418339562dc4916c67
Bmeshanko <bmeshank@purdue.edu>
2021-05-29 22:36:34 -0400

    Finished Binary Search

--- BinaryTree/BinarySearch.java
+++ BinaryTree/BinarySearch.java
@@ -1,20 +1,22 @@
 public class BinarySearch {
 	
-	public int search(int[] arr, int n, int low, int high) {
+	public static int search(int[] arr, int n, int low, int high) {
 		int mid = (low + high) / 2;
 
 		if (arr[mid] == n) return mid;
 
-		if (arr[mid] > n) search(arr, n, mid + 1, high);
+		if (arr[mid] < n) return search(arr, n, mid + 1, high);
 
-		if (arr[mid] < n) search(arr, n, low, mid);
+		if (arr[mid] > n) return search(arr, n, low, mid - 1);
+
+		return -1;
 	}
 
 	public static void main(String[] args) {
 		
 		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 		
-		int res = search(arr, 4, 0, arr.length());
+		int res = search(arr, 4, 0, arr.length - 1);
 		System.out.println(res);
 	}
 
